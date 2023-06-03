3d4d463c06eda15e24bbd148c0d6d63e77d9bfdd
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-06-02 22:00:25 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 package FabricaCarros;
 
+import FabricaCarros.factory.FactoryCarro;
+import FabricaCarros.model.Carros;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		FactoryCarro fabrica = new FactoryCarro();
+		
+		Carros[]carros = fabrica.criarTurma(FactoryCarro.SEDAN, 30);
+		Carros[]carros1 = fabrica.criarTurma(FactoryCarro.SUV, 10);
+		Carros[]carros2 = fabrica.criarTurma(FactoryCarro.PICKUP, 20);
+		Carros[]carros3 = fabrica.criarTurma(FactoryCarro.HATCH, 25);
+		
+		
 	}
 
 }
